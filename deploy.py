ab0920affc71670ac95e7393e7ffb444ddb9e275
chinmay18030 <77487404+chinmay18030@users.noreply.github.com>
2021-06-06 15:49:25 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,17 +1,15 @@
-import main
-import random
-import requests
-# import our module
-import json
+import createChatbot as cc
 
-data = main.load_data("C://Users//My//Desktop//Ace Tech Academy//Data//JSON//Artificial Intelligence.json")
+chatbot = cc.Chatbot("intents.json")
 
-x1,y1,words,document,classes = main.data_processing(data["intents"],"patterns","tag")
-model = main.train_model([[123,"relu"],[234,"relu"]],x1,y1)
+data = chatbot.load_data()
+
+x1,y1,words,document,classes = chatbot.data_processing(data["intents"],"patterns","tag")
+model = chatbot.train_model([[123,"relu"],[234,"relu"]],x1,y1)
 model.fit(x1,y1,epochs=200,batch_size=9)
-model.save("C://Users//My//Desktop//Ace Tech Academy//Models//Acrom.model")
+
 
 while True:
     asi = input("Message:")
-    ans = main.predict_class(model,asi.lower(),classes,words)
+    ans = chatbot.predict_class(model,asi.lower(),classes,words)
     print(ans)
